4ee31b10a9562243252be429ec5642a590c2b6ff
rsperoni <rsperoni@mgcoders.com>
2018-02-01 18:20:40 -0300

    cambios en deployment

---
+++ prepare_stuff.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#/bin/bash
+set -x
+mkdir -p /home/ubuntu/wp-"$DEPLOYMENT_GROUP_NAME"-files
+[ -z "$WORDPRESS_DB_PASSWORD" ] && echo "Need to set WORDPRESS_DB_PASSWORD" && exit 1;
+[ -z "$WORDPRESS_DB_HOST" ] && echo "Need to set WORDPRESS_DB_HOST" && exit 1;
+[ -z "$WORDPRESS_DB_USER" ] && echo "Need to set WORDPRESS_DB_USER" && exit 1;
+[ -z "$WORDPRESS_DB_NAME" ] && echo "Need to set WORDPRESS_DB_NAME" && exit 1;
+[ -z "$VIRTUAL_HOST" ] && echo "Need to set VIRTUAL_HOST" && exit 1;
+[ -z "$LETSENCRYPT_HOST" ] && echo "Need to set LETSENCRYPT_HOST" && exit 1;
+[ -z "$LETSENCRYPT_EMAIL" ] && echo "Need to set LETSENCRYPT_EMAIL" && exit 1;
+[ -z "$DEPLOYMENT_GROUP_NAME" ] && echo "Need to set APPLICATION_NAME" && exit 1;
+
+
--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #/bin/bash
 set -x
-cd /home/ubuntu/wp-titanium-deploy
-cp ../conf/wp-calcium-titanium.env /home/ubuntu/wp-titanium-deploy/.env
+chown -R www-data.www-data /home/ubuntu/wp-"$DEPLOYMENT_GROUP_NAME"-files/plugins
+chown -R www-data.www-data /home/ubuntu/wp-"$DEPLOYMENT_GROUP_NAME"-files/uploads
+cd /home/ubuntu/wp-"$DEPLOYMENT_GROUP_NAME"-deploy
+cp ../conf/"$DEPLOYMENT_GROUP_NAME".env /home/ubuntu/wp-"$DEPLOYMENT_GROUP_NAME"-deploy/.env
 echo docker-compose up
 docker-compose build && docker-compose up -d

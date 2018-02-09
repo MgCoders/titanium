d044edc13b61f5aab2eac5fd66925741d093bc80
rsperoni <rsperoni@mgcoders.com>
2018-02-09 15:57:03 -0300

    docker login

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,5 @@
 set -x
 cd /home/ubuntu/wp-REPLACE_PROJECT_NAME-deploy
 cp ../conf/wp-REPLACE_PROJECT_NAME-deploy.env /home/ubuntu/wp-REPLACE_PROJECT_NAME-deploy/.env
+$(aws ecr get-login --region us-east-1)
 docker-compose -f docker-compose.testing.yml pull && docker-compose -f docker-compose.testing.yml up -d

7cfc39b94dfe053ff7e30121f819b3386ec47b47
rsperoni <rsperoni@mgcoders.com>
2018-02-15 19:44:05 -0300

    lpm

--- stop_server.sh
+++ stop_server.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 #/bin/bash
 set -x
-cd /home/ubuntu/wp-REPLACE_PROJECT_NAME-deploy
-docker-compose -f docker-compose.testing.yml kill
-if [ $? -eq 0 ]
-then
-  echo "Successfull"
-  exit 0
-else
-  echo "Error" >&2
-  exit 1
-fi
+docker stack rm REPLACE_PROJECT_NAME
